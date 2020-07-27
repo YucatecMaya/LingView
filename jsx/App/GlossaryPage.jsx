d7c4d111c803302e60ecd10f4bfa50270214d7a6
maksymilian-dabkowski <maksymilian.michal.dabkowski@gmail.com>
2020-07-27 18:21:42 -0400

    Update GlossaryPage.jsx

--- jsx/App/GlossaryPage.jsx
+++ jsx/App/GlossaryPage.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 export function GlossaryPage() {
   return (
-      <div>
-          <p>Input glossary here :) </p>
-          <p>To customize this welcome text, edit the jsx/App/GlossaryPage.jsx file, then run webpack for your changes to take effect. </p>
+      <div style={{margin: "1in"}}>
+		<h2>De la transcripción</h2>
+		<p>Al inicio del proyecto se decidió hacer dos tipos de transcripción: una cercana al habla de los entrevistados (transcripción superficial) y otra que diera cuenta de las raíces y demás morfemas de la lengua (transcripción profunda). Esta decisión se tomó ya que cada una presenta ventajas y desventajas para distintos usuarios. Una transcripción más cercana al habla del entrevistado permite apreciar diversos procesos tales como: las elisiones, contracciones, asimilaciones, entre otras. Varios de estos fenómenos dan lugar a formas que no serían reconocibles para los no expertos. En cuanto a la transcripción profunda, ésta tiene la ventaja de representar morfemas fáciles de reconocer, y de encontrar en otras obras de referencia, como los diccionarios. Además, es posible a partir de ésta llevar a cabo otras tareas lingüísticas, como el corte morfológico y el glosado. Pero tiene la desventaja de no coincidir con el habla de los entrevistados. Veamos los siguientes ejemplos:</p>
+		<table>
+  			<tr>
+    			<td>transcripción superficial</td>
+    			<td><i>nojchu paylaile', pos maanlup'ée kiilo yiiki</i></td>
+  			</tr>
+  			<tr>
+    			<td>transcripción profunda</td>
+    			<td><i>nojoch u paylaile’, pos maanal jump’éel kiilo yiikil</i></td>
+  			</tr>
+    		<tr>
+    			<td>traducción</td>
+    			<td>está grande la payla, pues más de un kilo de chile</td>
+  			</tr>
+		</table>
+		<p>Ambas transcripciones se realizaron con un alfabeto práctico que diera cuenta de todos los fonemas del maya yucateco actual, por esta razón se contemplaron los grafemas “d”, “g”, y “f”, para los fonemas [d], [g], y [f], respectivamente. Estos fonemas se encuentran en palabras prestadas de otras lenguas, pero que ya están totalmente integradas en el maya. Por ejemplo:</p>
+		
       </div>
   );
 }
