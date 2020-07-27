bf9354396c1789cc5485089ae39253cfab893cbe
maksymilian-dabkowski <maksymilian.michal.dabkowski@gmail.com>
2020-07-27 16:06:24 -0400

    Update AboutPage.jsx

--- jsx/App/AboutPage.jsx
+++ jsx/App/AboutPage.jsx
@@ -4,7 +4,7 @@ export function AboutPage() {
   return (
       <div>
 		<h2>About U koorpusil maaya t’aan <br />
-		<span style={{font-size: "medium"}}><i>English</i></span></h2>
+		<i style={{fontSize: "medium"}}>English</i></h2>
 			<p><b>U koorpusil maaya t’aan</b> (English: <b>UNO-Brown Yucatec Maya Corpus</b>) is a searchable collection of audio and video texts in. Yucatec Maya is one of 30 modern Mayan languages, spoken by close to a million speakers primarily in the Mexican states of Yucatán, Quintana Roo, and Campeche.</p>
       </div>
   );
