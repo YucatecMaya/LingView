ebc0a8d0649c350830e15e845fdf08e2ecdcafb2
maksymilian-dabkowski <maksymilian.michal.dabkowski@gmail.com>
2020-07-27 16:05:14 -0400

    Update AboutPage.jsx

--- jsx/App/AboutPage.jsx
+++ jsx/App/AboutPage.jsx
@@ -5,7 +5,7 @@ export function AboutPage() {
       <div>
 		<h2>About U koorpusil maaya t’aan <br />
 		<span style={{font-size: "medium"}}><i>English</i></span></h2>
-			<p><b>U koorpusil maaya t’aan</b> (English: <b>UNO-Brown Yucatec Maya Corpus</b>) is a searchable collection of audio and video texts in <a href="https://en.wikipedia.org/wiki/Yucatec_Maya_language">Yucatec Maya</a>. Yucatec Maya is one of 30 modern Mayan languages, spoken by close to a million speakers primarily in the Mexican states of Yucatán, Quintana Roo, and Campeche.</p>
+			<p><b>U koorpusil maaya t’aan</b> (English: <b>UNO-Brown Yucatec Maya Corpus</b>) is a searchable collection of audio and video texts in. Yucatec Maya is one of 30 modern Mayan languages, spoken by close to a million speakers primarily in the Mexican states of Yucatán, Quintana Roo, and Campeche.</p>
       </div>
   );
 }
