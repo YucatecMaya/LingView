23cc902461b85233b152c5bf782a7513dbb41aff
Eli Sharf <esharf@google.com>
2020-11-29 14:11:04 -0800

    Change all text/jsx to allow for language select.

--- jsx/App/GlossaryPage.jsx
+++ jsx/App/GlossaryPage.jsx
@@ -1,22 +1,171 @@
 import React from 'react';
-import { ENGLISH, ESPANOL, FRANCAIS } from './locale/LocaleConstants.jsx';
+import { ESPANOL } from './locale/LocaleConstants.jsx';
 import { TranslatableText } from './locale/TranslatableText.jsx'
 
 const glossaryPageJSX = {
-  [ENGLISH] :
-    <div>
-      <p>Input glossary here :) </p>
-      <p>To customize this welcome text, edit the jsx/App/GlossaryPage.jsx file, then run webpack for your changes to take effect. </p>
-    </div>,
   [ESPANOL] :
     <div>
-      <p>Ingrese el glosario aquí :) </p>
-      <p>Para personalizar este texto de bienvenida, edite el archivo 'jsx/App/GlossaryPage.jsx' y luego ejecute el paquete web para que los cambios surtan efecto. </p>
-    </div>,
-  [FRANCAIS] :
-    <div>
-      <p>Saisissez le glossaire ici :) </p>
-      <p>Pour personnaliser ce texte de bienvenue, modifiez le fichier 'jsx/App/GlossaryPage.jsx', puis exécutez webpack pour que vos modifications prennent effet. </p>
+    <h2>De la transcripción</h2>
+		<p>Al inicio del proyecto se decidió hacer dos tipos de transcripción: una cercana al habla de los entrevistados (transcripción superficial) y otra que diera cuenta de las raíces y demás morfemas de la lengua (transcripción profunda). Esta decisión se tomó ya que cada una presenta ventajas y desventajas para distintos usuarios. Una transcripción más cercana al habla del entrevistado permite apreciar diversos procesos tales como: las elisiones, contracciones, asimilaciones, entre otras. Varios de estos fenómenos dan lugar a formas que no serían reconocibles para los no expertos. En cuanto a la transcripción profunda, ésta tiene la ventaja de representar morfemas fáciles de reconocer, y de encontrar en otras obras de referencia, como los diccionarios. Además, es posible a partir de ésta llevar a cabo otras tareas lingüísticas, como el corte morfológico y el glosado. Pero tiene la desventaja de no coincidir con el habla de los entrevistados. Veamos los siguientes ejemplos:</p>
+		<table>
+  			<tr>
+    			<td><b>transcripción superficial</b></td>
+    			<td><i>nojchu paylaile’, pos maanlup’ée kiilo yiiki</i></td>
+  			</tr>
+  			<tr>
+    			<td><b>transcripción profunda</b></td>
+    			<td><i>nojoch u paylaile’, pos maanal jump’éel kiilo yiikil</i></td>
+  			</tr>
+    		<tr>
+    			<td><b>traducción</b></td>
+    			<td>está grande la payla, pues más de un kilo de chile</td>
+  			</tr>
+		</table>
+		<p>Ambas transcripciones se realizaron con un alfabeto práctico que diera cuenta de todos los fonemas del maya yucateco actual, por esta razón se contemplaron los grafemas <i>d</i>, <i>g</i>, y <i>f</i>, para los fonemas [d], [g], y [f], respectivamente. Estos fonemas se encuentran en palabras prestadas de otras lenguas, pero que ya están totalmente integradas en el maya. Por ejemplo:</p>
+		<table>
+  			<tr>
+    			<td><b>transcripción superficial</b></td>
+    			<td><i>a laj desinfektaartik xan aantesa... a...</i></td>
+  			</tr>
+  			<tr>
+    			<td><b>transcripción profunda</b></td>
+    			<td><i>a láaj desinfektaartik xan aantes a... a...</i></td>
+  			</tr>
+    		<tr>
+    			<td><b>traducción</b></td>
+    			<td>a desinfectar todas antes de...</td>
+  			</tr>
+		</table>
+		<p>Para cada sonido del habla se usó en la escritura la misma letra o conjunto de letras (grafías), salvo en algunos casos en la que se transcribe siguiendo el español estándar (por ejemplo, NN). Se usaron las siguientes grafías en las transcripciones realizadas en el proyecto:</p>
+		<table>
+  			<tr>
+    			<td><b>fonema</b></td>
+    			<td>[a]</td>
+    			<td>[ɓ]</td>
+    			<td>[ʧ]</td>
+    			<td>[ʧʼ]</td>
+    			<td>[d]</td>
+    			<td>[e]</td>
+    			<td>[f]</td>
+    			<td>[g]</td>
+    			<td>[i]</td>
+    			<td>[h]</td>
+    			<td>[k]</td>
+    			<td>[k’]</td>
+    			<td>[l]</td>
+    			<td>[m]</td>
+    			<td>[n]</td>
+    			<td>[o]</td>
+    			<td>[p]</td>
+    			<td>[p’]</td>
+    			<td>[ɾ]</td>
+    			<td>[s]</td>
+    			<td>[t]</td>
+    			<td>[t’]</td>
+    			<td>[ʦ]</td>
+				<td>[ʦ’]</td>
+				<td>[u]</td>
+				<td>[w]</td>
+				<td>[ʃ]</td>
+				<td>[j]</td>
+  			</tr>
+  			<tr>
+  			    <td><b>grafía</b></td>
+    			<td><i>a</i></td>
+    			<td><i>b</i></td>
+    			<td><i>ch</i></td>
+    			<td><i>ch’</i></td>
+    			<td><i>d</i></td>
+    			<td><i>e</i></td>
+    			<td><i>f</i></td>
+    			<td><i>g</i></td>
+    			<td><i>i</i></td>
+    			<td><i>j</i></td>
+    			<td><i>k</i></td>
+				<td><i>k’</i></td>
+				<td><i>l</i></td>
+				<td><i>m</i></td>
+				<td><i>n</i></td>
+				<td><i>o</i></td>
+				<td><i>p</i></td>
+				<td><i>p’</i></td>
+				<td><i>r</i></td>
+				<td><i>s</i></td>
+				<td><i>t</i></td>
+				<td><i>t’</i></td>
+				<td><i>ts</i></td>
+				<td><i>ts’</i></td>
+				<td><i>u</i></td>
+				<td><i>w</i></td>
+				<td><i>x</i></td>
+				<td><i>y</i></td>
+			</tr>
+		</table>
+		<p>En el caso de las vocales se consideraron las siguientes formas:</p>
+		<table>
+  			<tr>
+  				<td><b>corta</b></td>
+    			<td><i>a</i></td>
+    			<td><i>e</i></td>
+    			<td><i>i</i></td>
+    			<td><i>o</i></td>
+    			<td><i>u</i></td>
+  			</tr>
+  			<tr>
+  				<td><b>larga</b></td>
+    			<td><i>aa</i></td>
+    			<td><i>ee</i></td>
+    			<td><i>ii</i></td>
+    			<td><i>oo</i></td>
+    			<td><i>uu</i></td>
+  			</tr>
+  			<tr>
+  				<td><b>alta</b></td>
+  				<td><i>áa</i></td>
+    			<td><i>ée</i></td>
+    			<td><i>íi</i></td>
+    			<td><i>óo</i></td>
+    			<td><i>úu</i></td>
+  			</tr>
+  			<tr>
+  				<td><b>rearticulada</b></td>
+  				<td><i>a’a</i></td>
+    			<td><i>e’e</i></td>
+    			<td><i>i’i</i></td>
+    			<td><i>o’o</i></td>
+    			<td><i>u’u</i></td>
+  			</tr>
+  			<tr>
+  				<td><b>glotalizada</b></td>
+  				<td><i>a’</i></td>
+    			<td><i>e’</i></td>
+    			<td><i>i’</i></td>
+    			<td><i>o’</i></td>
+    			<td><i>u’</i></td>
+  			</tr>
+  		</table>
+  		<p>Los antropónimos se transcribieron usando la ortografía oficial en español. Por ejemplo:</p>
+		<table>
+  			<tr>
+    			<td><b>transcripción</b></td>
+    			<td><i>in kaaba’e’ Martín</i></td>
+  			</tr>
+    		<tr>
+    			<td><b>traducción</b></td>
+    			<td>mi nombre es Martín</td>
+  			</tr>
+		</table>
+  		<p>Los topónimos se transcribieron a partir de la ortografía del maya y de acuerdo a su fonología. Por ejemplo:</p>
+  		<table>
+  			<tr>
+    			<td><b>transcripción</b></td>
+    			<td><i>in kaajale’ Póopla’</i></td>
+  			</tr>
+    		<tr>
+    			<td><b>traducción</b></td>
+    			<td>mi pueblo es Popolá</td>
+  			</tr>
+		</table>
     </div>,
 };
 
